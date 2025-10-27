feefcc7ad6b45708660c084eb1eba627d6c15945
fedorskarvinko <fedorskarvinko@gmail.com>
2025-10-27 20:01:25 +0400

    homework 14.2 done

--- tests/test_category.py
+++ tests/test_category.py
@@ -7,5 +7,15 @@ def test_category_init(category, first_product, second_product, third_product):
         category.description
         == "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни"
     )
+    assert category.products_in_list == [first_product, second_product, third_product]
     assert Category.category_count == 1
     assert Category.product_count == 3
+
+def test_category_products_property(category):
+    assert category.products == "Xiaomi Redmi Note 11, 31000.0 руб. Остаток: 14 шт.\n"
+
+
+def test_category_products_setter(category, first_product):
+    assert len(category.products_in_list) == 3
+    category.products_in_list = first_product
+    assert len(category.products_in_list) == 4
