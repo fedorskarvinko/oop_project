894f551d37ff8ed450d9478f558e58bfbaca29d1
fedorskarvinko <fedorskarvinko@gmail.com>
2025-10-30 02:08:17 +0400

    add new funcs in product.py and category.py

--- src/category.py
+++ src/category.py
@@ -1,3 +1,6 @@
+from src.product import Product
+
+
 class Category:
     name: str
     description: str
@@ -8,6 +11,36 @@ class Category:
     def __init__(self, name, description, products=None):
         self.name = name
         self.description = description
-        self.products = products if products else []
+        self.__products = products if products else []
         Category.category_count += 1
         Category.product_count += len(products) if products else 0
+
+    def __str__(self):
+        total_quantity = sum(product.quantity for product in self.__products)
+        return f"{self.name}, количество продуктов: {total_quantity} шт."
+
+    def add_product(self, product):
+        if isinstance(product, Product):
+            self.__products.append(product)
+            Category.product_count += 1
+
+    @property
+    def products(self):
+        products_str = ""
+        for product in self.__products:
+            products_str = f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+        return products_str
+
+    @products.setter
+    def products(self, product: Product):
+        self.__products.append(product)
+        Category.product_count += 1
+
+    @property
+    def products_in_list(self):
+        return self.__products
+
+    @products_in_list.setter
+    def products_in_list(self, product: Product):
+        self.__products.append(product)
+        Category.product_count += 1
--- src/product.py
+++ src/product.py
@@ -7,5 +7,33 @@ class Product:
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    def __str__(self):
+        return f"{self.name}, {self.price} руб. Остаток: {self.quantity} шт."
+
+    def __add__(self, other):
+        return self.__price * self.quantity + other.__price * other.quantity
+
+    @classmethod
+    def new_product(cls, product_dict):
+        return cls(**product_dict)
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price):
+        if new_price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+        elif self.__price >= new_price:
+            self.__price = new_price
+        else:
+            access = input("Подтвердите цену: y = да, n = нет: ")
+            print(access)
+            if access == "y":
+                self.__price = new_price
+            else:
+                print("Нет так нет")
