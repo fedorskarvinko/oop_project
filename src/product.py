500affb72e8722405f3bd7aa2dc4569cc6b6d3f1
fedorskarvinko <fedorskarvinko@gmail.com>
2025-11-11 02:21:36 +0400

    update product.py

--- src/product.py
+++ src/product.py
@@ -1,14 +1,20 @@
-class Product:
+from src.base_product import BaseProduct
+from src.print_mixin import PrintMixin
+
+
+class Product(BaseProduct, PrintMixin):
     name: str
     description: str
     price: float
     quantity: int
+
 
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
         self.__price = price
         self.quantity = quantity
+        super().__init__()
 
     def __str__(self):
         return f"{self.name}, {self.price} руб. Остаток: {self.quantity} шт."
